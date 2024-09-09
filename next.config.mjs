eab2a410ed9ccc57af684a36a0b9c61acbb08fa0
Eizus <suzie@alxdr.ca>
2024-09-09 12:58:49 -0400

    Revert "Add export to config"
    This reverts commit 763ff4cbc933a825e4cae843f0fc5b9f90d93323.

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,6 @@ import { unifiedConditional } from 'unified-conditional'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
